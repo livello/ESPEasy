52f7b9816d4789bc9e0d84b0d96c7dfe43263425
Sergey Konyukhov <live110.sk@gmail.com>
2019-09-18 14:08:21 +0300

    works smth

--- src/_P129_DS2484.ino
+++ src/_P129_DS2484.ino
@@ -4,51 +4,24 @@
 
 //	This is a modification to Plugin_004
 
-#include <DS2482_OneWire.h>
+#include <OneWire.h>
 #include <DallasTemperature.h>
-#include "ESPEasy_Log.h"
-#include "ESPEasy-Globals.h"
 
 #define PLUGIN_129
 #define PLUGIN_ID_129         129
-#define PLUGIN_NAME_129       "Environment - ds2482-100(I2C)ds18b20"
+#define PLUGIN_NAME_129       "Environment - DS18b20ds2484"
 #define PLUGIN_VALUENAME1_129 "Temperature"
 
 OneWire oneWire;
 DallasTemperature sensors(&oneWire);
 
+//sensors.begin();
 
+//uint8_t Plugin_129_DallasPin;
+//DeviceAddress 	currAddress;
 uint8_t 		numberOfDevices;
 #define DS2482_I2C_ADDRESS      0x18 // I2C address for the sensor
 
-
-void getDeviceCount() {
-    addLog(LOG_LEVEL_ERROR,F("getDeviceCount"));
-    sensors.begin();
-    numberOfDevices = sensors.getDeviceCount();
-}
-
-void applyDeviceCount() {
-    getDeviceCount();
-    switch(numberOfDevices){
-        case 0:
-            Device[deviceCount].VType              = SENSOR_TYPE_NONE;
-            break;
-        case 1:
-            Device[deviceCount].VType              = SENSOR_TYPE_SINGLE;
-            break;
-        case 2:
-            Device[deviceCount].VType              = SENSOR_TYPE_DUAL;
-            break;
-        case 3:
-            Device[deviceCount].VType              = SENSOR_TYPE_TRIPLE;
-            break;
-        default:
-            Device[deviceCount].VType              = SENSOR_TYPE_QUAD;
-
-    }
-    Device[deviceCount].ValueCount         = numberOfDevices;
-}
 
 boolean Plugin_129(byte function, struct EventStruct * event, String& string)
 {
@@ -57,16 +30,15 @@ boolean Plugin_129(byte function, struct EventStruct * event, String& string)
     switch (function)
     {
         case PLUGIN_DEVICE_ADD:
-           {
-            addLog(LOG_LEVEL_ERROR,F("PLUGIN_DEVICE_ADD"));
+        {
             Device[++deviceCount].Number           = PLUGIN_ID_129;
             Device[deviceCount].Type               = DEVICE_TYPE_I2C;
+            Device[deviceCount].VType              = SENSOR_TYPE_SINGLE;
             Device[deviceCount].Ports              = 0;
             Device[deviceCount].PullUpOption       = false;
             Device[deviceCount].InverseLogicOption = false;
-            Device[deviceCount].FormulaOption = false;
-            Device[deviceCount].DecimalsOnly = true;
-            applyDeviceCount();
+            Device[deviceCount].FormulaOption      = true;
+            Device[deviceCount].ValueCount         = 1;
             Device[deviceCount].SendDataOption     = true;
             Device[deviceCount].TimerOption        = true;
             Device[deviceCount].GlobalSyncOption   = true;
@@ -75,40 +47,21 @@ boolean Plugin_129(byte function, struct EventStruct * event, String& string)
 
         case PLUGIN_GET_DEVICENAME:
         {
-            addLog(LOG_LEVEL_ERROR,F("PLUGIN_GET_DEVICENAME"));
             string = F(PLUGIN_NAME_129);
             break;
         }
 
         case PLUGIN_GET_DEVICEVALUENAMES:
         {
-            addLog(LOG_LEVEL_ERROR,F("getDeviceValueNames"));
-            uint8_t tmpAddress[8];
-            byte count = 0;
-            for (int i=0; i<numberOfDevices; i++)
-            {
-                addLog(LOG_LEVEL_ERROR,F("GetAddress"));
-                sensors.getAddress(tmpAddress, i);
-                String option = "";
-                for (byte j = 0; j < 8; j++)
-                {
-                    option += String(tmpAddress[j], HEX);
-                    if (j < 7) option += F("_");
-                }
-                count ++;
-                ExtraTaskSettings.TaskDeviceValueDecimals[i]=2;
-                strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[i], option.c_str());
-            }
-
+            strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_129));
             break;
         }
 
         case PLUGIN_WEBFORM_LOAD:
         {
-            addLog(LOG_LEVEL_ERROR,F("WebFormLoad"));
             uint8_t savedAddress[8];
             byte resolutionChoice = 0;
-            addLog(LOG_LEVEL_ERROR,F("Sensors.begin"));
+            sensors.begin();
 
             // Scan the onewire bus and fill dropdown list with devicecount on this GPIO.
             //Plugin_129_DallasPin = Settings.TaskDevicePin1[event->TaskIndex];
@@ -122,7 +75,23 @@ boolean Plugin_129(byte function, struct EventStruct * event, String& string)
             addRowLabel(F("Device Address"));
             addSelector_Head(F("plugin_129_dev"), false);
             addSelector_Item("", -1, false, false, F(""));
+            uint8_t tmpAddress[8];
+            byte count = 0;
 
+			numberOfDevices = sensors.getDeviceCount();
+			for (int i=0; i<numberOfDevices; i++)
+			{
+				sensors.getAddress(tmpAddress, i);
+				String option = "";
+				for (byte j = 0; j < 8; j++)
+                {
+                    option += String(tmpAddress[j], HEX);
+                    if (j < 7) option += F("-");
+                }
+				bool selected = (memcmp(tmpAddress, savedAddress, 8) == 0) ? true : false;
+                addSelector_Item(option, count, selected, false, F(""));
+                count ++;
+			}
 			/*
             while (Plugin_129_DS_search(tmpAddress))
             {
@@ -143,11 +112,11 @@ boolean Plugin_129(byte function, struct EventStruct * event, String& string)
             if (ExtraTaskSettings.TaskDevicePluginConfigLong[0] != 0)
                 resolutionChoice = Plugin_129_DS_getResolution(savedAddress);
             else
-                resolutionChoice = 11;
+                resolutionChoice = 9;
             String resultsOptions[4] = { "9", "10", "11", "12" };
             int resultsOptionValues[4] = { 9, 10, 11, 12 };
             addFormSelector(F("Device Resolution"), F("plugin_129_res"), 4, resultsOptions, resultsOptionValues, resolutionChoice);
-            addHtml(F(" Bit"));
+            string += F(" Bit");
 
             success = true;
             break;
@@ -155,7 +124,6 @@ boolean Plugin_129(byte function, struct EventStruct * event, String& string)
 
         case PLUGIN_WEBFORM_SAVE:
         {
-            addLog(LOG_LEVEL_ERROR,F("WebFormSave"));
             uint8_t addr[8] = {0,0,0,0,0,0,0,0};
 
             // save the address for selected device and store into extra tasksettings
@@ -185,43 +153,38 @@ boolean Plugin_129(byte function, struct EventStruct * event, String& string)
 
         case PLUGIN_READ:
         {
-            float value[8];
             if (ExtraTaskSettings.TaskDevicePluginConfigLong[0] != 0){
-                addLog(LOG_LEVEL_ERROR,F("PLUGIN_READ"));
-                applyDeviceCount();
+                uint8_t addr[8];
                 // Load ROM address from tasksettings
                 LoadTaskSettings(event->TaskIndex);
-                uint8_t tmpAddress[8];
-                for (int i=0; i<numberOfDevices; i++)
+                for (byte x = 0; x < 8; x++)
+                    addr[x] = ExtraTaskSettings.TaskDevicePluginConfigLong[x];
+
+                //Plugin_129_DallasPin = Settings.TaskDevicePin1[event->TaskIndex];
+                float value = 0;
+                String log  = F("DS   : Temperature: ");
+                if (Plugin_129_DS_readTemp(addr, &value))
                 {
-                    addLog(LOG_LEVEL_ERROR,F("GetAddress"));
-                    sensors.getAddress(tmpAddress, i);
-                    String option = "";
-                    addLog(LOG_LEVEL_ERROR,F("Form option-address"));
-                    for (byte j = 0; j < 8; j++)
-                    {
-                        option += String(tmpAddress[j], HEX);
-                        if (j < 7) option += F("_");
-                    }
-                    addLog(LOG_LEVEL_ERROR,F("send TaskDeviceValueNames"));
-                    strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[i], option.c_str());
-                    addLog(LOG_LEVEL_ERROR,F("set decimals"));
-                    ExtraTaskSettings.TaskDeviceValueDecimals[i]=2;
-                    option+= F(" : Temperature: ");
-                    addLog(LOG_LEVEL_ERROR,F("Read Value"));
-                    if (Plugin_129_DS_readTemp(tmpAddress, &value[i]))
-                    {
-                        option    += value[i];
-                    }
-                    else
-                    {
-                        value[i] = NAN;
-                        option += F("Error!");
-                    }
-                    addLog(LOG_LEVEL_INFO, option);
-                    UserVar[event->BaseVarIndex+i] = value[i];
+                    UserVar[event->BaseVarIndex] = value;
+                    log    += UserVar[event->BaseVarIndex];
+                    success = true;
                 }
-                success = true;
+                else
+                {
+                    UserVar[event->BaseVarIndex] = NAN;
+                    log += F("Error!");
+                }
+
+                log += (" (");
+                for (byte x = 0; x < 8; x++)
+                {
+                    if (x != 0)
+                        log += "-";
+                    log += String(ExtraTaskSettings.TaskDevicePluginConfigLong[x], HEX);
+                }
+
+                log += ')';
+                addLog(LOG_LEVEL_INFO, log);
             }
             break;
         }
@@ -248,9 +211,7 @@ byte Plugin_129_DS_scan(byte getDeviceROM, uint8_t* ROM)
 \*********************************************************************************************/
 boolean Plugin_129_DS_readTemp(uint8_t ROM[8], float * value)
 {
-    addLog(LOG_LEVEL_ERROR,F("sensors.requestTemperaturesByAddress(ROM)"));
   sensors.requestTemperaturesByAddress(ROM);
-    addLog(LOG_LEVEL_ERROR,F("value = sensors.getTempC(ROM);"));
 	*value = sensors.getTempC(ROM);
 
     return true;
